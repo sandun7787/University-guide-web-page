11fd52cff465cc6a347f2fd9f7828fedbc8f0fd2
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-09-21 04:25:09 -0700

    add-the-pointer-and-cursors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
-
+import New from './Components/New/New'
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className='container'>
+      <New/>
+      </div>
+     
+      
+      
     </div>
   )
 }

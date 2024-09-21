feda571d3bd589d7f657783ff6d1f126572f6b16
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-09-21 09:05:29 -0700

    modified-the-about-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import New from './Components/New/New'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 export default function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ export default function App() {
       <div className='container'>
         <Title subTitle='Our PROGRAM' title='What We Offer'/>
       <New/>
+      <About/>
       </div>
      
       
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+        <div className="about-left">
+          <img src={about_img} alt="" className='about-img' />
+          <img src={play_icon} alt="" className='play-icon' />
+
+        </div>
+
+        <div className="about-right">
+            <h3>ABOUT UNIVERSITY</h3>
+            <h2>The University of Moratuwa (UoM) is one of Sri Lanka's</h2>
+            <p>The University of Moratuwa (UoM) is one of Sri Lanka's leading technological universities, renowned for its focus on engineering, technology, and design. Situated in the suburbs of Colombo, UoM offers undergraduate and postgraduate programs across a variety of fields, including engineering, architecture, and information technology.
+
+The university prides itself on producing highly skilled graduates who are well-prepared for the demands of the global job market. It fosters innovation, research, and industry collaboration, with a strong emphasis on entrepreneurship and applied learning. UoM is known for its state-of-the-art facilities, dedicated faculty, and its contribution to Sri Lanka's development through research and technological advancements.</p>
+        </div>
     </div>
   )
 }

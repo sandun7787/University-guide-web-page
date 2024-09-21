5917e921803df99b15af4a6ffc1b996fd37d0010
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-09-21 08:00:17 -0700

    add-new-about-page-and-resole-bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import New from './Components/New/New'
+import Title from './Components/Title/Title'
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <div className='container'>
+        <Title subTitle='Our PROGRAM' title='What We Offer'/>
       <New/>
       </div>
      

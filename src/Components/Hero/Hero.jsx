d3e6c95a13c695011f5b455e07bc4fb6241b4345
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-09-21 01:13:30 -0700

    add the expolore button and complete the front end ui

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero container">
+      <div className='hero-text'>
+        <h1>  We Ensure better educations for the better world</h1>
+        <p>A circulation or lending department is one of the key departments of a library. The main public service point is the circulation desk or loans desk, usually found near the main entrance of a library. </p>
+        <button className='btn'>Explore More<img src={dark_arrow}></img></button>
+
+      </div>
       
     </div>
   )

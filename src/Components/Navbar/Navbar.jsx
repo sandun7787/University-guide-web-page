f1eb718887b08eb1816e96e8921feeb85e760385
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-09-21 21:23:43 -0700

    correct-the-ajax

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import "./Navbar.css"
-import logo from '../../assets/logo.png'
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 
 export default function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll); // Cleanup on unmount
+    };
+  }, []);
+
   return (
-   <nav className='container dark-nav'>
-    <img src={logo} alt="" className='logo'/>
-    <ul>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}> {/* Corrected string interpolation */}
+      <img src={logo} alt="Logo" className="logo" />
+      <ul>
         <li>Home</li>
         <li>Program</li>
         <li>About Us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li><button className='btn'>Contact Us</button></li>
-    </ul>
-   </nav>
-  )
+        <li><button className="btn">Contact Us</button></li>
+      </ul>
+    </nav>
+  );
 }

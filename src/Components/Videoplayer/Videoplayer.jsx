66cbaf96faee56a4582c01082aa559bb5d6bae85
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-09-23 03:46:38 -0700

    update

---
+++ src/Components/Videoplayer/Videoplayer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Videoplayer.css'
+
+const Videoplayer = () => {
+  return (
+    <div className='video-player'>
+        <video src={}></video>
+      
+    </div>
+  )
+}
+
+export default Videoplayer
